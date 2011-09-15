39a71734bd8482542e62b9c22808a746b50863d3
Luiz Branco <artfikan@gmail.com>
2011-09-15 17:01:49 -0300

    Heroku fixes - 4th attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Trollspeak::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = on
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
