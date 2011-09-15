ad313974055cdae02c3d6c8340fd3617b2a8d7bd
Luiz Branco <artfikan@gmail.com>
2011-09-15 17:49:26 -0300

    Heroku fixes - almost given up 10

--- app/controllers/speeches_controller.rb
+++ app/controllers/speeches_controller.rb
@@ -12,6 +12,7 @@ respond_to :html, :xml, :json
     escape_phrase = CGI.escape(@speech.phrase)
     url = "http://translate.google.com/translate_tts?tl=#{@speech.lang}&q=#{escape_phrase}"
     shorten_url = BITLY_CLIENT.shorten(url)
+    @speech.url = shorten_url.short_url
     if @speech.save
       #flash[:message] = @speech.url
       redirect_to speeches_url
